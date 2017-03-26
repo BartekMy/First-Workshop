d2cdd8a0fc3f7372c81539fd972348974ece41b3
Bartosz Mylka <bmylka@gmail.com>
2017-03-26 17:26:04 +0200

    Update JS-file top-menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+var nav = document.getElementById('second_level');
+var button = document.getElementById('top_menu');
+
+console.log(nav, button);
+
+button.addEventListener("mouseover", function(){
+  if()
+
+});
+
 
 
 });
