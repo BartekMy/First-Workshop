7995fb7c8240225974cbbd2d1718a10ba3634a3e
Bartosz Mylka <bmylka@gmail.com>
2017-03-26 18:55:07 +0200

    Second update JS-file top-menu

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-var nav = document.getElementById('second_level');
-var button = document.getElementById('top_menu');
+var children = document.getElementById('drop_menu');
+var parent = document.getElementById('top_menu');
 
-console.log(nav, button);
+parent.addEventListener("mouseover", showChildrenElement);
+parent.addEventListener("mouseout", hideChildrenElement);
 
-button.addEventListener("mouseover", function(){
-  if()
-
-});
-
-
+function hideChildrenElement() {
+    children.style.display = "none";
+}
+function showChildrenElement() {
+    children.style.display = "block";
+}
 
 });
