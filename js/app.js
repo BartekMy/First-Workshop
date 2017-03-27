d70fff1d11ba3e78c85130bdc229303c04403f13
Bartosz Mylka <bmylka@gmail.com>
2017-03-27 18:55:44 +0200

    Textfield and slider added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+//top_menu
 var children = document.getElementById('drop_menu');
 var parent = document.getElementById('top_menu');
 
@@ -11,6 +12,55 @@ function hideChildrenElement() {
 }
 function showChildrenElement() {
     children.style.display = "block";
+}
+
+// textfield1 and textfield2
+var textfield1 = document.querySelector('.textfield1');
+var box1 = document.getElementById('box1');
+
+box1.addEventListener("mouseover", hideChildren);
+box1.addEventListener("mouseout", showChildren);
+
+function hideChildren() {
+    textfield1.style.display = "none";
+}
+function showChildren() {
+    textfield1.style.display = "block";
+}
+
+var textfield2 = document.querySelector('.textfield2');
+var box2 = document.getElementById('box2');
+
+box2.addEventListener("mouseover", hideChild);
+box2.addEventListener("mouseout", showChild);
+
+function hideChild() {
+    textfield2.style.display = "none";
+}
+function showChild() {
+    textfield2.style.display = "block";
+}
+
+// Slider
+var buttons = document.querySelectorAll('.button');
+var sliderImg = document.querySelectorAll('#chairs');
+var imgNumber = 0;
+
+sliderImg[0].classList.add('visible');
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', change)
+}
+function change(e) {
+  console.log(e.target.id);
+    var prevNumber = imgNumber;
+    e.target.id === "sign_right" ? imgNumber++ : imgNumber--;
+    if (imgNumber < 0) {
+        imgNumber = sliderImg.length - 1;
+    } else if (imgNumber > sliderImg.length - 1) {
+        imgNumber = 0;
+    }
+    sliderImg[prevNumber].classList.remove('visible');
+    sliderImg[imgNumber].classList.add('visible');
 }
 
 });
